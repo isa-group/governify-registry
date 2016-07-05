e93b597e483a85afe4939d688dab3d8932d2a206
Juanlu <juanlu.delafuente7@gmail.com>
2016-07-05 19:40:53 +0200

    async config

--- controllers/states/guarantees/guarantees.js
+++ controllers/states/guarantees/guarantees.js
@@ -4,10 +4,10 @@ var jsyaml = require('js-yaml');
 var $RefParser = require('json-schema-ref-parser');
 var config = require('../../../config');
 var stateManager = require('../../../stateManager/stateManager.js')
+var Promise = require("bluebird");
 var fs = require('fs');
 var errorModel = require('../../../errors/index.js').errorModel;
 var logger = config.logger;
-
 
 module.exports.guaranteesGET = function(args, res, next) {
     /**
@@ -23,32 +23,61 @@ module.exports.guaranteesGET = function(args, res, next) {
         id: agreementId
     }).then((manager) => {
         logger.ctlState("Getting state of guarantees...");
-        var processGuarantees = [];
-        manager.agreement.terms.guarantees.forEach(function(guarantee) {
-            processGuarantees.push(manager.get('guarantees', {
-                guarantee: guarantee.id
-            }));
-        });
 
-        Promise.all(processGuarantees).then(function(guaranteesValues) {
-            var result = [];
-            try {
-                guaranteesValues.forEach(function(guaranteeValues) {
-                    var res = guaranteeValues.map(function(guaranteeValue) {
-                        return manager.current(guaranteeValue);
+        if (config.async.guarantees) {
+            logger.ctlState("Processing guarantees in async mode");
+            var processGuarantees = [];
+            manager.agreement.terms.guarantees.forEach(function(guarantee) {
+                processGuarantees.push(manager.get('guarantees', {
+                    guarantee: guarantee.id
+                }));
+            });
+
+            Promise.all(processGuarantees).then(function(guaranteesValues) {
+                var result = [];
+                try {
+                    guaranteesValues.forEach(function(guaranteeValues) {
+                        var res = guaranteeValues.map(function(guaranteeValue) {
+                            return manager.current(guaranteeValue);
+                        });
+                        result = result.concat(res);
                     });
-                    result = result.concat(res);
-                });
-                res.json(result);
-            } catch (err) {
+                    res.json(result);
+                } catch (err) {
+                    logger.error(err);
+                    res.status(500).json(new errorModel(500, err));
+                }
+            }, function(err) {
                 logger.error(err);
                 res.status(500).json(new errorModel(500, err));
-            }
-        }, function(err) {
-            logger.error(err);
-            res.status(500).json(new errorModel(500, err));
-        });
-
+            });
+        } else {
+            logger.ctlState("Processing guarantees in sync mode");
+            var ret = [];
+            Promise.each(manager.agreement.terms.guarantees, (guarantee) => {
+                logger.ctlState("- guaranteeId: " + guarantee.id);
+                // var guaranteeParams = args.scope.value;
+                // guaranteeParams.period = guaranteeParams.period ? guaranteeParams.period : {
+                //     from: '*',
+                //     to: '*'
+                // };
+                // guaranteeParams.guarantee = guarantee.id;
+                return manager.get('guarantees', {
+                    guarantee: guarantee.id
+                }).then((results) => {
+                    for (var i in results) {
+                        ret.push(manager.current(results[i]));
+                    }
+                }, (err) => {
+                    logger.error(err);
+                });
+            }).then(function(results) {
+                res.json(ret);
+            }, (err) => {
+                logger.error("ERROR processing guarantees: ", err);
+                res.status(500).json(new errorModel(500, err));
+            });
+        }
     }, (err) => {
         logger.error(err);
         res.status(500).json(new errorModel(500, err));
