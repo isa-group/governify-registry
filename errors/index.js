aba1ab4f60a2c381e6867867e9aba6b9857898a4
Daniel Arteaga <darteaga@us.es>
2016-09-30 12:37:02 +0200

    refactor error model and added documentation of constructor

--- errors/index.js
+++ errors/index.js
@@ -1,6 +1,21 @@
 'use strict';
 
-module.exports.errorModel = function (code, message){
+module.exports.errorModel = Error;
+
+/**
+ * Error model for error response.
+ *
+ * Examples:
+ *
+ *    return new Error(500, 'Internal server error')
+ *
+ * @param {Integer} code
+ * @param {String} message
+ *
+ * @return {Error} Object
+ * @api public
+ */
+function Error(code, message){
     this.code = code;
     this.message = message;
 }
