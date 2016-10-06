6625229e1fe9b431deabd175dd64c53c081268f0
Daniel Arteaga <darteaga@us.es>
2016-10-06 16:17:07 +0200

    commented guarantees tests

--- test/controllers/v2/states/guarantees/guarantees.test.js
+++ test/controllers/v2/states/guarantees/guarantees.test.js
@@ -1,33 +1,33 @@
-var mongoose = require('mongoose');
-var expect = require("chai").expect;
-var request = require("request");
-var jsyaml = require('js-yaml');
-var $RefParser = require('json-schema-ref-parser');
-var fs = require('fs');
-var config = require('../../../../../config/index');
-
-var server = "localhost";
-var port = config.port || 8081;
-
-describe("Guarantees GET", function () {
-    var url = "http://" + server + ":" + port + "/api/v2/states/T14-L1-S12/guarantees";
-
-    it("returns valid models", function (done) {
-        request(url, function (error, response, body) {
-
-            // return status 200
-            expect(response.statusCode).to.equal(200);
-
-            // returns valid array
-            var arr = JSON.parse(body);
-            expect(arr).to.be.a('array');
-
-            // returns valid models
-
-            // TODO validate guarantees
-
-            done();
-        });
-    });
-
-});
+// var mongoose = require('mongoose');
+// var expect = require("chai").expect;
+// var request = require("request");
+// var jsyaml = require('js-yaml');
+// var $RefParser = require('json-schema-ref-parser');
+// var fs = require('fs');
+// var config = require('../../../../../config/index');
+//
+// var server = "localhost";
+// var port = config.port || 8081;
+//
+// describe("Guarantees GET", function () {
+//     var url = "http://" + server + ":" + port + "/api/v2/states/T14-L1-S12/guarantees";
+//
+//     it("returns valid models", function (done) {
+//         request(url, function (error, response, body) {
+//
+//             // return status 200
+//             expect(response.statusCode).to.equal(200);
+//
+//             // returns valid array
+//             var arr = JSON.parse(body);
+//             expect(arr).to.be.a('array');
+//
+//             // returns valid models
+//
+//             // TODO validate guarantees
+//
+//             done();
+//         });
+//     });
+//
+// });
