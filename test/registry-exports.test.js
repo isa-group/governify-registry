e538c1b09ccb62c2ce3f16c0a141a7db7e337937
Daniel Arteaga <darteaga@us.es>
2016-10-06 16:15:19 +0200

    added module exports tests

---
+++ test/registry-exports.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var expect = require('chai').expect,
+    request = require('request');
+
+describe('Registry Module Tests', () => {
+    var registry = require('../index');
+    it('.deploy() Function', () => {
+        expect(registry.deploy).to.not.null;
+    });
+});
