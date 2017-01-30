55304a33b12ba4ec72cc4cad09edb90d37031ad4
Daniel Arteaga <darteaga@us.es>
2017-01-30 16:15:20 +0100

    udated all files with license

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,23 @@
+/*!
+governify-registry 0.0.1, built on: 2017-01-30
+Copyright (C) 2017 ISA Group
+http://www.isa.us.es/
+http://registry.governify.io/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 'use strict';
 module.exports = function (grunt) {
 
@@ -22,16 +42,16 @@ module.exports = function (grunt) {
             encoding: 'utf8'
         }).toString(),
         usebanner: {
-            // license: {
-            //     options: {
-            //         position: 'top',
-            //         banner: '/*!\n<%= license %>*/\n',
-            //         replace: true
-            //     },
-            //     files: {
-            //         src: ['**/*.js']
-            //     }
-            // },
+            license: {
+                options: {
+                    position: 'top',
+                    banner: '/*!\n<%= license %>*/\n',
+                    replace: true
+                },
+                files: {
+                    src: ['**/*.js']
+                }
+            },
             readme: {
                 options: {
                     position: 'bottom',
--- utils/timeAndPeriod.js
+++ utils/timeAndPeriod.js
@@ -1,3 +1,23 @@
+/*!
+governify-registry 0.0.1, built on: 2017-01-30
+Copyright (C) 2017 ISA Group
+http://www.isa.us.es/
+http://registry.governify.io/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 'use strict';
 var config = require('../config');
 var logger = config.logger;
